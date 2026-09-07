76b2873e98a4ecfcc32da41b408a701ae874aaa3
Dania Macias <dlm4695@rit.edu>
2026-09-06 23:18:42 -0400

    Implemented some UI requirements

--- D3D11Starter-main/Game.cpp
+++ D3D11Starter-main/Game.cpp
@@ -11,6 +11,12 @@
 #pragma comment(lib, "d3dcompiler.lib")
 #include <d3dcompiler.h>
 
+// This code assumes files are in "ImGui" subfolder!
+// Adjust as necessary for your own folder structure and project setup
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_dx11.h"
+#include "ImGui/imgui_impl_win32.h"
+
 // For the DirectX Math library
 using namespace DirectX;
 
@@ -47,6 +53,16 @@ Game::Game()
 		Graphics::Context->VSSetShader(vertexShader.Get(), 0, 0);
 		Graphics::Context->PSSetShader(pixelShader.Get(), 0, 0);
 	}
+
+	// Initialize ImGui itself & platform/renderer backends
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplWin32_Init(Window::Handle());
+	ImGui_ImplDX11_Init(Graphics::Device.Get(), Graphics::Context.Get());
+	// Pick a style (uncomment one of these 3)
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsLight();
+	//ImGui::StyleColorsClassic();
 }
 
 
@@ -58,7 +74,10 @@ Game::Game()
 // --------------------------------------------------------
 Game::~Game()
 {
-	// May use later
+	// ImGui clean up
+	ImGui_ImplDX11_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
 }
 
 
@@ -232,15 +251,55 @@ void Game::CreateGeometry()
 // --------------------------------------------------------
 void Game::OnResize()
 {
-	printf("Resize\n");
+
 }
 
+
+// ImGui Helper Method
+void ImGuiUpdate(float deltaTime) {
+	// Feed fresh data to ImGui
+	ImGuiIO& io = ImGui::GetIO();
+	io.DeltaTime = deltaTime;
+	io.DisplaySize.x = (float)Window::Width();
+	io.DisplaySize.y = (float)Window::Height();
+	// Reset the frame
+	ImGui_ImplDX11_NewFrame();
+	ImGui_ImplWin32_NewFrame();
+	ImGui::NewFrame();
+	// Determine new input capture
+	Input::SetKeyboardCapture(io.WantCaptureKeyboard);
+	Input::SetMouseCapture(io.WantCaptureMouse);
+	// Show the demo window
+	ImGui::ShowDemoWindow();
+}
 
 // --------------------------------------------------------
 // Update your game here - user input, move objects, AI, etc.
 // --------------------------------------------------------
 void Game::Update(float deltaTime, float totalTime)
 {
+	// Call helper method to update ImGui
+	ImGuiUpdate(deltaTime);
+
+	ImGui::Begin("Inspector"); // Everything after is part of the windo
+
+	// Replace the %f with the next parameter, and format as a float
+	ImGui::Text("Framerate: %f fps", ImGui::GetIO().Framerate);
+	// Replace each %d with the next parameter, and format as decimal integers
+	// The "x" will be printed as-is between the numbers, like so: 800x600
+	ImGui::Text("Window Resolution: %dx%d", Window::Width(), Window::Height());
+
+	XMFLOAT4 color(1.0f, 0.0f, 0.5f, 1.0f);
+	ImGui::ColorEdit4("RGBA color editor", &color.x);
+
+	// Create a button and test for a click
+	if (ImGui::Button("Show ImGui Demo Window"))
+	{
+		
+	}
+
+	ImGui::End(); // Ends the current window
+
 	// Example input checking: Quit if the escape key is pressed
 	if (Input::KeyDown(VK_ESCAPE))
 		Window::Quit();
@@ -294,6 +353,8 @@ void Game::Draw(float deltaTime, float totalTime)
 	{
 		// Present at the end of the frame
 		bool vsync = Graphics::VsyncState();
+		ImGui::Render(); // Turns this frame’s UI into renderable triangles
+		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData()); // Draws it to the screen
 		Graphics::SwapChain->Present(
 			vsync ? 1 : 0,
 			vsync ? 0 : DXGI_PRESENT_ALLOW_TEARING);
